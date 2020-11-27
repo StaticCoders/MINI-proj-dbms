e32a36f473c1574274fb08fc013524f48b17e0c8
tejasanerao <tejasanerao75@gmail.com>
2020-11-27 15:27:39 +0530

    Changes made in main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from PyQt5.QtWidgets import QMessageBox
 from HomePage import *
 from chooseInBatches import *
@@ -10,24 +9,26 @@ from Registration import *
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
+        self.batchAllotment = Ui_BatchAllotmentWindow()
+        self.batches = Ui_BatchesWindow()
+        self.newRegistration = Ui_NewRegistrationWindow()
+        self.registration = Ui_RegistrationWindow()
+        self.homescreen = Ui_MainWindow()
         self.startHome()
 
     # HOME
 
     def startHome(self):
-        self.homescreen = Ui_MainWindow()
         self.homescreen.setupUi(self)
         self.setWindowTitle("Home")
         self.homescreen.registration.clicked.connect(self.startRegistration)
         self.homescreen.Batches.clicked.connect(self.startBatches)
-        self.homescreen.Payment.clicked.connect(self.startPayment)
         self.hide()
         self.showMaximized()
 
     # REGISTRATION
 
     def startRegistration(self):
-        self.registration = Ui_RegistrationWindow()
         self.registration.setupUi(self)
         self.setWindowTitle("Registration")
         self.registration.newRegistrationButton.clicked.connect(self.startNewRegistration)
@@ -36,17 +37,15 @@ class MainWindow(QtWidgets.QMainWindow):
         self.showMaximized()
 
     def startNewRegistration(self):
-        self.newregistration = Ui_NewRegistrationWindow()
-        self.newregistration.setupUi(self)
+        self.newRegistration.setupUi(self)
         self.setWindowTitle("New Registration")
-        self.newregistration.cancelButton.clicked.connect(self.startRegistration)
+        self.newRegistration.cancelButton.clicked.connect(self.startRegistration)
         self.hide()
         self.showMaximized()
 
     # BATCHES
 
     def startBatches(self):
-        self.batches = Ui_BatchesWindow()
         self.batches.setupUi(self)
         self.setWindowTitle("Batches")
         self.hide()
@@ -56,11 +55,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.batches.cancelButton.clicked.connect(self.startHome)
 
     def startBatchAllotment(self):
-        self.batchAllotment = Ui_BatchAllotmentWindow()
         self.batchAllotment.setupUi(self)
         self.hide()
         self.showMaximized()
         self.batchAllotment.cancelButton.clicked.connect(self.startBatches)
+
 
 if __name__ == "__main__":
     import sys
